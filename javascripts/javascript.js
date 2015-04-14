a55bc4bbd34536ca790ff46c102e4b65e53b2c7e
Kyle Charters <kylewcharters@gmail.com>
2015-04-13 23:39:46 -0400

    f

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,9 +1,10 @@
 /*jslint browser: true*/
 /*global $*/
 
-// Initialize Collapse Button
-$('.button-collapse').sideNav({activationWidth: 50});
 // Initialize Tabs
 $(document).ready(function(){
   $('ul.tabs').tabs();
 });
+
+// Initialize Collapse Button
+$('.button-collapse').sideNav({activationWidth: 50});
